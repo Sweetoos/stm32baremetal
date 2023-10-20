51d26b4b34e144e1ca2de5aab21c3ce8cbe0599b
kacper <thespbsik@gmail.com>
2023-10-20 15:04:34 +0200

    simple UART_TX project

---
+++ uart_project1/Debug/Src/main.d
@@ -0,0 +1,19 @@
+Src/main.o: ../Src/main.c \
+ /home/kacper/mycodes/stm32/STM32F446RE/bare_metal/uart_project1/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ /home/kacper/mycodes/stm32/STM32F446RE/bare_metal/uart_project1/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f446xx.h \
+ /home/kacper/mycodes/stm32/STM32F446RE/bare_metal/uart_project1/Drivers/CMSIS/Include/core_cm4.h \
+ /home/kacper/mycodes/stm32/STM32F446RE/bare_metal/uart_project1/Drivers/CMSIS/Include/cmsis_version.h \
+ /home/kacper/mycodes/stm32/STM32F446RE/bare_metal/uart_project1/Drivers/CMSIS/Include/cmsis_compiler.h \
+ /home/kacper/mycodes/stm32/STM32F446RE/bare_metal/uart_project1/Drivers/CMSIS/Include/cmsis_gcc.h \
+ /home/kacper/mycodes/stm32/STM32F446RE/bare_metal/uart_project1/Drivers/CMSIS/Include/mpu_armv7.h \
+ /home/kacper/mycodes/stm32/STM32F446RE/bare_metal/uart_project1/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ /home/kacper/mycodes/stm32/STM32F446RE/bare_metal/uart_project1/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f446xx.h
+/home/kacper/mycodes/stm32/STM32F446RE/bare_metal/uart_project1/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+/home/kacper/mycodes/stm32/STM32F446RE/bare_metal/uart_project1/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f446xx.h:
+/home/kacper/mycodes/stm32/STM32F446RE/bare_metal/uart_project1/Drivers/CMSIS/Include/core_cm4.h:
+/home/kacper/mycodes/stm32/STM32F446RE/bare_metal/uart_project1/Drivers/CMSIS/Include/cmsis_version.h:
+/home/kacper/mycodes/stm32/STM32F446RE/bare_metal/uart_project1/Drivers/CMSIS/Include/cmsis_compiler.h:
+/home/kacper/mycodes/stm32/STM32F446RE/bare_metal/uart_project1/Drivers/CMSIS/Include/cmsis_gcc.h:
+/home/kacper/mycodes/stm32/STM32F446RE/bare_metal/uart_project1/Drivers/CMSIS/Include/mpu_armv7.h:
+/home/kacper/mycodes/stm32/STM32F446RE/bare_metal/uart_project1/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+/home/kacper/mycodes/stm32/STM32F446RE/bare_metal/uart_project1/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f446xx.h:
